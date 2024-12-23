0266fce4306ddfd813e04c6cae1e3b164496b527
Ryan Wang <ziyuanwang.cn@outlook.com>
2024-12-23 22:43:56 +0800

    Initialize user model and database connection

---
+++ test/testGorm.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"heychat/models"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/heyChat2?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("fail to connect to the database")
+	}
+	// fmt.Println("Connect to the database")
+
+	// 传递的参数应该是指针
+	// 根据提供的模型结构体，自动创建或更新数据库中的表
+	db.AutoMigrate(&models.UserBasic{})
+
+	user := &models.UserBasic{
+		Name:          "王五",
+		LoginTime:     uint64(time.Now().Unix()),
+		HeartbeatTime: uint64(time.Now().Unix()),
+		LogOutTime:    uint64(time.Now().Unix()),
+	}
+	// fmt.Println(reflect.TypeOf(user))
+
+	db.Create(user)
+
+	db.Model(user).Update("PassWord", "1234")
+}
