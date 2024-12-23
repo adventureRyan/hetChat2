abb9d28358e8f944532e2f0e396b81108207ea69
Ryan Wang <ziyuanwang.cn@outlook.com>
2024-12-23 23:09:34 +0800

    Adding routing setup and index point

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+import "heychat/router"
+
 func main() {
-	
+	r := router.Router()
+	r.Run(":8081")
 }
---
+++ service/index.go
@@ -0,0 +1,10 @@
+package service
+
+import "github.com/gin-gonic/gin"
+
+// gin.Context 是 Gin 框架中的核心对象，包含了 HTTP 请求的上下文，例如请求信息、响应信息和中间件的数据。
+func GetIndex(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "welcome!!",
+	})
+}
