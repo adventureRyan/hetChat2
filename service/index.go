2d9c7bdefe07305ae3123e386bf075ea301d06fc
Ryan Wang <ziyuanwang.cn@outlook.com>
2024-12-25 00:42:49 +0800

    Adding Swag and add API comments to GetIndex & GetUserList

--- service/index.go
+++ service/index.go
@@ -5,6 +5,10 @@ import (
 )
 
 // gin.Context 是 Gin 框架中的核心对象，包含了 HTTP 请求的上下文，例如请求信息、响应信息和中间件的数据。
+// GetIndex
+// @Tags 首页
+// @Success 200 {string} welcome
+// @Router /index [get]
 func GetIndex(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "welcome!!",
--- service/userservice.go
+++ service/userservice.go
@@ -6,6 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// GetUserList
+// @Summary 用户列表
+// @Tags 用户模块
+// @Success 200 {string} json{"code", "message"}
+// @Router /user/getUserList [get]
 func GetUserList(c *gin.Context) {
 	data := make([]*models.UserBasic, 10)
 	data = models.GetUserList()
